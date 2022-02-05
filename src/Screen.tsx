e5ac632fd0f14f4ffb867b89f058a207ad06e4ec
Danielo Rodriguez <rdanielo@gmail.com>
2022-02-05 18:42:40 +0100

    chore: minnor screen update

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -1,12 +1,10 @@
 import { createMemo } from "solid-js";
 
 export function Screen(props: { width: number; height: number }) {
-  const style = createMemo(
-    () => `height: ${props.height}px; width: ${props.width}px;`
-  );
+  const style = () => `height: ${props.height}px; width: ${props.width}px;`;
   return (
     <div
-      className="bg-gray-500 rounded border-cyan-600 border-2"
+      class="bg-gray-500 rounded border-cyan-600 border-2 relative shadow-md"
       style={style()}
     >
       screen
