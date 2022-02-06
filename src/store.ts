50a1ac48c13e47c604559fdfeccc9822c22d3df0
Danielo Rodriguez <rdanielo@gmail.com>
2022-02-06 17:20:32 +0100

    chore: store addElement, getNextID and compile

--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,54 @@
 import { createStore } from "solid-js/store";
+import { HaspButton } from "./haspButton";
+
+type PageElement = HaspButton;
 
 const initialState = {
   currentPage: 1,
-  pages: [],
+  pages: [] as PageElement[],
   layout: "horizontal",
+  jsonL: "",
+  get currentPageElements() {
+    return this.pages.filter(
+      (element) => element.page === this.currentPage || element.page === 0
+    );
+  },
 };
 
 const [store, setStore] = createStore(initialState);
 
+const getNextId = (page: number) => {
+  const currentPageIds = store.pages
+    .filter((element) => element.page === page)
+    .map((element) => element.id)
+    .concat(0);
+  return Math.max(...currentPageIds) + 1;
+};
+
 const nextPage = () => {
   setStore("currentPage", (current) => current + 1);
 };
+
 const prevPage = () => {
   setStore("currentPage", (current) => Math.max(current - 1, 1));
+};
+
+const addElement = (element) => {
+  setStore("pages", (pages) => [
+    ...pages,
+    { ...element, id: getNextId(store.currentPage), page: store.currentPage },
+  ]);
+};
+
+const compile = () => {
+  const output = store.pages.map((element) => JSON.stringify(element));
+  setStore("jsonL", output.join("\n"));
 };
 
 export default {
   store,
   nextPage,
   prevPage,
+  addElement,
+  compile,
 };
