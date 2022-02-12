38041494bd66b0b822f0239f442e356a9fe2edbb
Danielo Rodriguez <rdanielo@gmail.com>
2022-02-12 21:50:01 +0100

    chore: button sizes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,16 @@ interface ButtonProps {
   label: string;
   variant: "default" | "primary";
   onClick: () => void;
+  size?: "sm" | "md" | "lg";
 }
 
 export function Button(p: ButtonProps) {
   const classList = {
     "btn-primary": p.variant === "primary",
     "btn-outline": p.variant === "default",
+    "btn-sm": p.size === "sm",
+    "btn-md": p.size === "md",
+    "btn-lg": p.size === "lg",
   };
   return (
     <button class="btn" classList={classList} onClick={p.onClick}>
